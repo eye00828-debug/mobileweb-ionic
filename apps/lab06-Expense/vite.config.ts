936fa4fc23b83a9c4092a65ed92552712496b6d7
Kh <eye00828@gmail.com>
2026-02-10 00:59:08 +0700

    fix router for github pages (hash mode)

--- apps/lab06-Expense/vite.config.ts
+++ apps/lab06-Expense/vite.config.ts
@@ -7,15 +7,20 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 🔥 สำคัญมาก สำหรับ GitHub Pages (repo: mobileweb-ionic / folder: lab06)
+  base: '/mobileweb-ionic/lab06/',
+
   plugins: [
     vue(),
     legacy()
   ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   test: {
     globals: true,
     environment: 'jsdom'
