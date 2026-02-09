fd1ec57b65de900528fe1ee7fd6b2eea87a6221c
Kh <eye00828@gmail.com>
2026-02-09 23:53:02 +0700

    update mobileweb project (all labs)

--- node_modules/.pnpm/node_modules/lab01_vue/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'lab01_vue',
-  webDir: 'dist'
-};
-
-export default config;
--- node_modules/.pnpm/node_modules/lab04-photo-vue/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'lab04-photo-vue',
-  webDir: 'dist'
-};
-
-export default config;
